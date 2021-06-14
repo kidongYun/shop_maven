794a52f9a451fda6b34c44c278fdf77478c356d1
Kidong Yun <oriondigestive@gmail.com>
2021-06-14 23:27:00 +0900

    JPA

---
+++ src/main/java/com/kianyun/shop_maven/ShopMavenApplication.java
@@ -0,0 +1,13 @@
+package com.kianyun.shop_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMavenApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/kianyun/shop_maven/model/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.kianyun.shop_maven.model.entity;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
